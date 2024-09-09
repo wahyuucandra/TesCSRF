dd2c227b659285d7e0c5c3488d9bccea6d833fed
wahyuucandra <wahyuucandra@gmail.com>
2024-09-09 14:10:42 +0700

    fix: csrf

--- app/page.tsx
+++ app/page.tsx
@@ -26,38 +26,12 @@ export default function Page() {
       <p>NOTE: Look at browser network logs and server console for submission feedback</p>
       <h3>Example 1:</h3>
       <form action={myAction}>
-        <legend>Form without CSRF (should fail):</legend>
-        <input type="text" name="input1" />
-        <button type="submit">Submit</button>
-      </form>
-      <br />
-      <form action={myAction}>
-        <legend>Form with incorrect CSRF (should fail):</legend>
-        <input type="hidden" name="csrf_token" value="notvalid" />
-        <input type="text" name="input1" />
-        <button type="submit">Submit</button>
-      </form>
-      <br />
-      <form action={myAction}>
         <legend>Form with CSRF (should succeed):</legend>
         <input type="hidden" name="csrf_token" value={csrfToken} />
         <input type="text" name="input1" />
         <button type="submit">Submit</button>
       </form>
       <h3>Example 2:</h3>
-      <form action={myAction}>
-        <legend>Form without CSRF (should fail):</legend>
-        <input type="file" name="file1" />
-        <button type="submit">Submit</button>
-      </form>
-      <br />
-      <form action={myAction}>
-        <legend>Form with incorrect CSRF (should fail):</legend>
-        <input type="hidden" name="csrf_token" value="notvalid" />
-        <input type="file" name="file1" />
-        <button type="submit">Submit</button>
-      </form>
-      <br />
       <form action={myAction}>
         <legend>Form with CSRF (should succeed):</legend>
         <input type="hidden" name="csrf_token" value={csrfToken} />
